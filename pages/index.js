9f1b41b2f12de980139338d88fa16c1514faa077
kenny-io <ekeneeze3@gmail.com>
2021-12-16 22:46:27 +0400

    load pets

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,26 @@ const Show = ({ show }) => (
 
     <hr />
 
-    <h1>Show #{show.id}</h1>
-    <p>{show.name}</p>
+    {show.map((pet, i) => (
+      <div key={i} className="p-4 sm:w-1/2 md:w-1/3 lg:w-1/4 ">
+        <div className="h-full bg-gradient-to-r from-red-500 to-yellow-500 hover:from-yellow-400 hover:to-red-400 rounded-lg overflow-hidden">
+          <img
+            className="lg:h-96 md:h-36 w-full object-cover object-center"
+            src={pet.image}
+            alt="pets"
+          />
+          <div className="p-6 items-center mx-auto">
+            <h1 className="title-font text-lg font-medium mb-3 font-pumpkin">
+              {pet.name}, {pet.age} / {pet.location}
+            </h1>
+            <p className="leading-relaxed mb-3 font-sfpro">{pet.funfact}</p>
+            <button className="flex mt-8 mb-8 mx-auto text-center font-bold py-2 px-4 rounded-full border border-white  font-pumpkin ">
+              Connect with {pet.name}
+            </button>
+          </div>
+        </div>
+      </div>
+    ))}
 
     <hr />
 
@@ -18,14 +36,14 @@ const Show = ({ show }) => (
 );
 
 export async function getStaticProps(context) {
-  const res = await fetch(`https://api.tvmaze.com/shows/71`);
+  const res = await fetch(`https://petscabndate.glitch.me`);
   const data = await res.json();
 
   return {
     props: {
       show: data,
     },
-    revalidate: 1,
+    revalidate: 20,
   };
 }
 
